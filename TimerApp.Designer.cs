1ac36aa94ecf5edd2efe5ee349fda5cb05ec488e
ObsequiumGlobal <86030157+ObsequiumGlobal@users.noreply.github.com>
2021-06-17 08:50:04 +0530

    Add files via upload

---
+++ TimerApp.Designer.cs
@@ -0,0 +1,78 @@
+﻿
+namespace TimerTestApp
+{
+    partial class TimerApp
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btn_Timer = new System.Windows.Forms.Button();
+            this.txt_Timer = new System.Windows.Forms.RichTextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // btn_Timer
+            // 
+            this.btn_Timer.BackColor = System.Drawing.Color.LightBlue;
+            this.btn_Timer.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btn_Timer.Location = new System.Drawing.Point(314, 643);
+            this.btn_Timer.Name = "btn_Timer";
+            this.btn_Timer.Size = new System.Drawing.Size(117, 52);
+            this.btn_Timer.TabIndex = 0;
+            this.btn_Timer.Text = "Timer";
+            this.btn_Timer.UseVisualStyleBackColor = false;
+            this.btn_Timer.Click += new System.EventHandler(this.btn_Timer_Click);
+            // 
+            // txt_Timer
+            // 
+            this.txt_Timer.Location = new System.Drawing.Point(12, 12);
+            this.txt_Timer.Name = "txt_Timer";
+            this.txt_Timer.Size = new System.Drawing.Size(420, 625);
+            this.txt_Timer.TabIndex = 1;
+            this.txt_Timer.Text = "";
+            // 
+            // TimerApp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(444, 707);
+            this.Controls.Add(this.txt_Timer);
+            this.Controls.Add(this.btn_Timer);
+            this.Name = "TimerApp";
+            this.Text = "Timer - Application";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Timer;
+        private System.Windows.Forms.RichTextBox txt_Timer;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ TimerApp.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TimerTestApp
+{
+    public partial class TimerApp : Form
+    { 
+        private bool stopProcess = false; // Boolean flag used to stop the process
+
+        public TimerApp()
+        {
+            InitializeComponent();
+            this.btn_Timer.BackColor = Color.LightBlue;
+        }
+        private void btn_Timer_Click(object sender, EventArgs e)
+        {
+            if (stopProcess != true)
+            {
+                this.btn_Timer.BackColor = Color.Green;
+                this.btn_Timer.ForeColor = Color.White;
+                stopProcess = true; // Flag to indicate the button event has been started
+                timer1.Tick += new EventHandler(timer1_Tick); //New handler is created to print the date + time
+                timer1.Interval = 2000; // in miliseconds
+                timer1.Start(); 
+            }
+            else
+            {
+                this.btn_Timer.BackColor = Color.LightBlue;
+                this.btn_Timer.ForeColor = Color.Black;
+                stopProcess = false; // Flag to indicate the button event has been stopped
+                timer1.Stop();
+                timer1.Tick -= new EventHandler(timer1_Tick);  //existing handler is Removed so that multiple events are not created
+            }
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            txt_Timer.Text = DateTime.UtcNow.ToString("dd MMM yyyy hh:mm:ss") + Environment.NewLine + txt_Timer.Text; //Prints the date with time on top of earlier text.
+        }
+    }
+}
